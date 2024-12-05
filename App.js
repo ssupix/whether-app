061623a6627ec0724d381a66e53169681054a1ab
ssupix <151896598+ssupix@users.noreply.github.com>
2024-12-04 16:10:48 -0800

    stack navigator + details screen

--- App.js
+++ App.js
@@ -1,25 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 
-// fonts
+// Fonts
 import { useFonts } from 'expo-font';
 
-// theme
-import { ThemeProvider, Text } from '@rneui/themed';
+// Theme
+import { ThemeProvider } from '@rneui/themed';
 import { theme } from './style/theme';
 
-// Import each tab
+// Import Screens
 import Home from './components/Home';
 import Search from './components/Search';
 import SavedLocations from './components/SavedLocations';
 import Details from './components/Details';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
+// Tab Navigator
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color }) => {
+        let iconName;
+        if (route.name === 'Home') {
+          iconName = 'cloud-outline';
+        } else if (route.name === 'Search') {
+          iconName = 'search-outline';
+        } else if (route.name === 'SavedLocations') {
+          iconName = 'bookmark-outline';
+        }
+        return <Ionicons name={iconName} size={30} color={color} />;
+      },
+      tabBarActiveTintColor: theme.colors.orange,
+      tabBarInactiveTintColor: theme.colors.darkGrey,
+      tabBarStyle: {
+        backgroundColor: '#D0CCC7',
+        borderTopWidth: 0,
+        height: 60,
+        paddingTop: 10,
+      },
+      tabBarShowLabel: false,
+      headerShown: false,
+    })}
+  >
+    <Tab.Screen name="Home" component={Home} />
+    <Tab.Screen name="Search" component={Search} />
+    <Tab.Screen name="SavedLocations" component={SavedLocations} />
+  </Tab.Navigator>
+);
+
+// App Component
 const App = () => {
   let [fontsLoaded] = useFonts({
     'RethinkSans_Normal': require('./assets/fonts/RethinkSans-VariableFont_wght.ttf'),
@@ -34,39 +69,22 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <SafeAreaProvider>
         <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
-
-                if (route.name === 'Home') {
-                  iconName = 'cloud-outline';
-                } else if (route.name === 'Search') {
-                  iconName = 'search-outline';
-                } else if (route.name === 'SavedLocations') {
-                  iconName = 'bookmark-outline';
-                }
-
-                // Return the appropriate Ionicons
-                return <Ionicons name={iconName} size={30} color={color} />;
-              },
-              tabBarActiveTintColor: theme.colors.orange,
-              tabBarInactiveTintColor: theme.colors.darkGrey,
-              headerShown: false, // Optionally hide header for all screens
-              tabBarShowLabel: false,
-              tabBarStyle: {
-                backgroundColor: '#D0CCC7',
-                borderTopWidth: 0,
-                alignItems: 'center',
-                paddingTop: 10,
-                height: 60,
-              },
-            })}
-          >
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Search" component={Search} />
-            <Tab.Screen name="SavedLocations" component={SavedLocations} />
-          </Tab.Navigator>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Tabs"
+              component={TabNavigator}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Details"
+              component={Details}
+              options={({ route }) => ({
+                title: route.params.item.name, // Dynamically set the title
+                headerStyle: { backgroundColor: theme.colors.grey },
+                headerTintColor: theme.colors.white,
+              })}
+            />
+          </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaProvider>
     </ThemeProvider>
---
+++ data/weatherQuotes.js
@@ -0,0 +1,12 @@
+export const weatherQuotes = [
+    "I wanted to catch fog, but I mist...",
+    "I tried to make a joke about the wind, but it blows.",
+    "You know it’s cold outside when you go outside and it’s cold.",
+    "Clouds are just nature’s way of throwing shade.",
+    "The best thing one can do when it’s raining is to let it rain.",
+    "Lightning never strikes twice... unless you’re really unlucky.",
+    "I told my umbrella we weren’t working out. It was too shady.",
+    "Weather forecast for tonight: dark.",
+    "The sun and I had a fight, but it burned me.",
+    "My snowman’s career? A total meltdown."
+];
